a0d44515322a7b795ab3a100141501c807ba99df
AsiaKot <joanna.kotwasinska@gmail.com>
2021-11-29 15:30:25 +0000

    optimize code (delete special chars list; use ascii dec instead)

--- ciphers/rot13.py
+++ ciphers/rot13.py
@@ -2,15 +2,11 @@ from typing import List, Union, NoReturn
 
 
 class Rot13:
-    special_chars: List[str] = [" ", ",", ".", "!", "?", ":", ";", "-", "_", "+",
-                                "(", ")", "|", "^", "@", "#", "%", "&", "*", "/",
-                                "<", ">", "{", "}", "=", "'", "\"", "\\",
-                                "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 
     def __init__(self, text: str) -> NoReturn:
         self.text = text
 
-    def use_cipher(self) -> Union[str,]: # jak nazwać 2gi typ pliku w Union: "False"? bo funkcja może zwrócić też False
+    def use_cipher(self) -> Union[str,]:  # jak nazwać 2gi typ pliku w Union: "False"? bo funkcja może zwrócić też False
         """
         Encrypt/decrypt text with ROT13 cipher that replaces a letter
         with the 13th letter after it in the alphabet.
@@ -20,19 +16,17 @@ class Rot13:
         text_to_cipher: List[str] = list(self.text)
         cipher_chars: List[str] = []
         for char in text_to_cipher:
-            if char in self.special_chars:
-                cipher_chars.append(char)
+            ascii_dec = ord(char)
+            if ascii_dec in [*range(32, 65), *range(91, 97), *range(123, 127)]:
+                cipher_letter = str(char)
+            elif ascii_dec in [*range(65, 78), *range(97, 110)]:
+                cipher_letter = chr(ascii_dec + 13)
+            elif ascii_dec in [*range(78, 91), *range(110, 123)]:
+                cipher_letter = chr(ascii_dec - 13)
             else:
-                ascii_dec = ord(char)
-                if ascii_dec in range(65, 78) or ascii_dec in range(97, 110):
-                    cipher_letter = chr(ascii_dec + 13)
-                    cipher_chars.append(cipher_letter)
-                elif ascii_dec in range(78, 91) or ascii_dec in range(110, 123):
-                    cipher_letter = chr(ascii_dec - 13)
-                    cipher_chars.append(cipher_letter)
-                else:
-                    print("Zastosowano niedozwolony znak")
-                    return False
+                print("Zastosowano niedozwolony znak")
+                return False
+            cipher_chars.append(cipher_letter)
 
         cipher_text: str = "".join(cipher_chars)
         return cipher_text
